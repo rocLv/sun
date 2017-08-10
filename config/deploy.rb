5e112fb2fb1d252aaa22990fd5ab0043d86649cf
rocLv <wangqsh999@gmail.com>
2017-08-10 09:26:29 +0800

    Set up project

---
+++ config/deploy.rb
@@ -0,0 +1,18 @@
+set :application, "Sun"
+set :repo_url, "https://github.com/rocLv/sun.git"
+
+# Project-specific overrides go here.
+# For list of variables that can be customized, see:
+# https://github.com/mattbrictson/capistrano-mb/blob/master/lib/capistrano/tasks/defaults.rake
+
+fetch(:mb_recipes) << "sidekiq"
+fetch(:mb_aptitude_packages)["redis-server@ppa:chris-lea/redis-server"] = :redis
+
+set :mb_dotenv_keys, %w[
+  rails_secret_key_base
+  postmark_api_key
+  sidekiq_web_username
+  sidekiq_web_password
+]
+
+after "deploy:published", "bundler:clean"
